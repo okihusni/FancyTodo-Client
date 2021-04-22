af6ebe163b403b162557bca4c3944168ad70cf1e
okihusni <okihusni@gmail.com>
2021-04-22 22:18:41 +0700

    progressing register and login

--- index.js
+++ index.js
@@ -1,26 +1,85 @@
 $(document).ready(() => {
+
+  $('#go-to-login').on('click', (e) => {
+    e.preventDefault();
+    $('#register').hide();
+    $('#login').show();
+  })
+
+  $('#go-to-register').on('click', (e) => {
+    e.preventDefault();
+    $('#register').show();
+    $('#login').hide();
+  })
+  
+  isLoggedIn();
+
   $('#register-form').on('submit', (e) => {
     e.preventDefault();
     register();
   });
+  
+  $('#login-form').on('submit', (e) => {
+    e.preventDefault();
+    login();
+  })
 });
 
 const register = () => {
-  const inputEmailRegister = $('#emailRegister');
-  const inputPassRegister = $('#passwordRegister');
+  const email = $('#emailRegister').val();
+  const password = $('#passwordRegister').val();
 
   $.ajax({
-    method: 'post',
+    method: 'POST',
     url: 'http://localhost:3000/users/register',
     data: {
-      email = inputEmailRegister.val(),
-      password = inputPassRegister.val()
+      email,
+      password
+    }
+  })
+  .done(() => {
+    $('#emailRegister').val('');
+    $('#passwordRegister').val('');
+  })
+  .fail(err => {
+    const { errors } = err.responseJSON;
+    console.log(errors);
+  });
+};
+
+const isLoggedIn = () => {
+  if(localStorage.getItem('access_token')) {
+    $('#register').hide();
+    $('#login').hide();
+    $('#todos').show();
+  }
+  else {
+    $('#register').show();
+    $('#login').hide();
+    $('#todo').hide();
+  }
+}
+
+const login = () => {
+  const email = $('#email').val();
+  const password = $('#password').val();
+
+  $.ajax({
+    method: 'POST',
+    url: 'http://localhost:3000/users/login',
+    data: {
+      email,
+      password
     }
   })
   .done(data => {
-    console.log(data);
+    const { access_token } = data;
+    localStorage.setItem('access_token', access_token);
+    $('#email').val('');
+    $('#password').val('');
   })
   .fail(err => {
-    console.log(err);
+    const { errors } = err.responseJSON;
+    console.log(errors);
   });
 };
