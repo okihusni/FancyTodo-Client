d2670208e50c05cbef66071ad108c59c1cbec11e
okihusni <okihusni@gmail.com>
2021-04-22 15:11:46 +0700

    revision for html structur and working on jquery

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 $(document).ready(() => {
-  console.log('cek masuk gak');
-})
+  $('#register-form').on('submit', (e) => {
+    e.preventDefault();
+    register();
+  });
+});
+
+const register = () => {
+  const inputEmailRegister = $('#emailRegister');
+  const inputPassRegister = $('#passwordRegister');
+
+  $.ajax({
+    method: 'post',
+    url: 'http://localhost:3000/users/register',
+    data: {
+      email = inputEmailRegister.val(),
+      password = inputPassRegister.val()
+    }
+  })
+  .done(data => {
+    console.log(data);
+  })
+  .fail(err => {
+    console.log(err);
+  });
+};
